cd2cb4afef85fd9103e1b500fca420bf8cc790c9
alpturan-dev <abdurrahmanalpturan13@hotmail.com>
2023-05-11 16:13:30 +0200

    edit home page

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Open_Sans } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const open_Sans = Open_Sans({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Abdurrahman Alpturan',
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={open_Sans.className}>{children}</body>
     </html>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,56 @@
 import Image from 'next/image'
+import ben from '@/public/assets/ben.png'
+import IconTwitter from "@/public/assets/twitter";
+import IconYoutube from "@/public/assets/youtube";
+import IconGithub from "@/public/assets/github";
+import Link from 'next/link';
+import Head from 'next/head';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-20">
 
-    </main>
+  const SOCIAL = {
+    email: "abdurrahmanalpturan13@gmail.com",
+    github: "https://github.com/alpturan-dev",
+    youtube: "https://www.youtube.com/@abdurrahmanalpturan",
+    twitter: "https://twitter.com/alpturandev"
+  }
+
+  return (
+    <div>
+      <Head>
+        <meta name="description" content="Abdurrahman Alpturan" />
+        <meta name="keywords" content="Abdurrahman Alpturan, Alpturan Abdurrahman" />
+        <meta name="author" content="Abdurrahman Alpturan" />
+      </Head>
+      <main className="pt-40 flex min-h-screen flex-col items-center gap-10">
+        <div className="w-3/5 flex flex-col lg:flex-row items-center justify-center gap-6 lg:gap-10 xl:gap-10">
+          <div className="w-3/4 sm:w-2/4 md:w-2/5 rounded">
+            <Image className="rounded-lg saturate-0 transition-all duration-700 hover:saturate-100" src={ben} width={800} height={600} alt="Abdurrahman Alpturan" />
+          </div>
+          <div className="w-full md:w-4/5 lg:w-full text-center lg:text-left">
+            <p className="text-2xl lg:text-3xl font-semibold">Merhaba, ben Abdurrahman.</p>
+            <p className="text-xs lg:text-sm font-light">Web Developer</p>
+            <div className="flex gap-4 md:gap-6 items-center justify-center lg:justify-start pt-10">
+              <Link className="hover:opacity-70" href={SOCIAL.twitter}>
+                <IconTwitter />
+              </Link>
+
+              <Link className="hover:opacity-70" href={SOCIAL.youtube}>
+                <IconYoutube />
+              </Link>
+
+              <Link className="hover:opacity-70" href={SOCIAL.github}>
+                <IconGithub />
+              </Link>
+
+              <Link className="hover:opacity-70" href={`mailto:${SOCIAL.email}`}>
+                <span className="text-sm">Email</span>
+              </Link>
+
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
   )
 }
