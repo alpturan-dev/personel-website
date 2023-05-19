cfe4092741beff7abf17537ce243fa0c3fd7796a
alpturan-dev <abdurrahmanalpturan13@hotmail.com>
2023-05-19 17:47:23 +0200

    fix responsive

--- app/bookmarks/page.js
+++ app/bookmarks/page.js
@@ -6,24 +6,27 @@ export default async function Bookmarks() {
     var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     return (
         <main className="pt-6 w-full mx-auto min-h-screen">
-            <h1 className="text-2xl text-center font-bold py-6 underline">Bookmarks</h1>
+            <h1 className="text-xl text-center font-bold py-2 underline">Bookmarks</h1>
             {recipes.results.map((item, index) => (
                 <div
                     key={index}
-                    className="mx-auto my-2 w-3/5 py-4 px-8 border rounded-sm border-slate-400 bg-slate-100 shadow-md"
+                    className="w-5/5 sm:w-4/5 md:w-3/5 lg:w-2/5 mx-1 sm:mx-auto my-2 py-4 px-8 bg-slate-100 rounded-sm shadow-md hover:shadow-xl transition-all"
                 >
-                    <div className="font-bold text-lg">
+                    <div className="font-semibold text-base">
                         {item.properties.Name.title.map((name) => (
                             name.plain_text
                         ))}
                     </div>
-                    <a
-                        key={index}
-                        href={item.properties.URL.url}
-                        className="opacity-60 hover:underline text-sm"
-                    >
-                        {item.properties.URL.url} | {formatDate(item.created_time)}
-                    </a>
+                    <div className="opacity-60 font-light text-sm">
+                        <a
+                            key={index}
+                            href={item.properties.URL.url}
+                            className="hover:underline"
+                        >
+                            {item.properties.URL.url}
+                        </a>
+                        <p className="pt-2 flex justify-end font-medium">{formatDate(item.created_time)}</p>
+                    </div>
                 </div>
             ))}
         </main>
